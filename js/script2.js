3b2019fa0d02e806bdc465052ff0a589bfaa3812
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-04-26 19:32:44 +0200

    Ready for first guiding

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,7 @@
 //Blog details 
 
 const blogContainer = document.querySelector(".details");
-const detailsTitle = document.querySelector(".detailTitle");
+const detailTitle = document.querySelector(".detailTitle");
 console.log(blogContainer);
 
 const QueryString = document.location.search;
@@ -12,7 +12,27 @@ const id = Params.get("id");
 
 console.log(id);
 
-const ThisURL = "https://bluenord.no/wp-json/wp/v2/posts" + id;
+const ThisURL = "https://bluenord.no/wp-json/wp/v2/posts/" + id;
 
 console.log(ThisURL);
 
+async function fetchBlog() {
+    try {
+        const resp = await fetch(ThisURL);
+        const blogDetails = await resp.json();
+        console.log(blogDetails);
+
+        detailTitle.innerHTML = `${blogDetails.title.rendered}`
+
+        blogContainer.innerHTML = `<h1>${blogDetails.title.rendered}</h1>
+        <p>${blogDetails.content.rendered}</p>
+        `
+
+    }
+    catch(error) {
+        console.log(error);
+        ResultContainer.innerHTML = "Obs, something whent wrong!";
+    }
+}
+
+fetchBlog()
