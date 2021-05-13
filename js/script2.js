65d79cd9b711e862c772a42b425a1a46979ca701
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-05-13 16:29:42 +0200

    Fixed loader

--- js/script2.js
+++ js/script2.js
@@ -36,8 +36,6 @@ async function fetchBlog() {
         <img class="blog-spesific-img" src="${blogImgDetails._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url}">
         <div class="blog-spesific-p">${blogDetails.content.rendered}</div>
         </div>   
-    
-
         `
 
     }
