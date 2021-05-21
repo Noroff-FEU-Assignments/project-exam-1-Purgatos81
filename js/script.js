dee93a6cad766a4326856012bc844dfce3bc58ab
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-05-21 13:59:13 +0200

    Working on wiew more button
    Added javascript to blogs page for more content

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 
 const blogResults = document.querySelector(".blogs");
+const moreBlogResults = document.querySelector(".more-blogs");
 const bluenordBlogsApi = "https://bluenord.no/wp-json/wp/v2/posts";
 const bluenordEmbedApi = "https://bluenord.no/wp-json/wp/v2/posts/?_embed=wp:featuredmedia";
 
@@ -20,7 +21,7 @@ async function getBlogs() {
         const blogs = jsonres;
         const blogsImg = jsonres2;
 
-        for (let i = 0; i < blogs.length; i++) {
+        for (let i = 0; i < 8; i++) {
             blogResults.innerHTML += 
             `<a class="blog-a-link" href="blogdetails.html?id=${blogs[i].id}">
                 <img class="blog-img" src="${blogsImg[i]._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url}"> 
@@ -36,3 +37,32 @@ async function getBlogs() {
 }
 
 getBlogs();
+
+async function getMoreBlogs() {
+    try {
+        const response = await fetch(bluenordBlogsApi);
+        const response2 = await fetch(bluenordEmbedApi);
+        const jsonres = await response.json();
+        const jsonres2 = await response2.json();
+        console.log(jsonres);
+        console.log(jsonres2);
+        moreBlogResults.innerHTML = "";
+        const blogs = jsonres;
+        const blogsImg = jsonres2;
+
+        for (let i = 8; i < blogs.length; i++) {
+            moreBlogResults.innerHTML += 
+            `<a class="blog-a-link" href="blogdetails.html?id=${blogs[i].id}">
+                <img class="blog-img" src="${blogsImg[i]._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url}"> 
+            <h2 class="blog-head"> ${blogs[i].title.rendered}</h2>
+            <div id="blog-p"> ${blogs[i].excerpt.rendered}</div>
+            </a>`
+        }
+    }
+    catch(error) {
+        console.log(error);
+        blogResults.innerHTML ="Obs, something whent wrong!"
+    }
+}
+
+getMoreBlogs();
